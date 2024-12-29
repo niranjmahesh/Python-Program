cb767a8984b85718a4410141ee27f44b4bc54454
niranjmahesh <niranjmahesh@gmail.com>
2024-12-29 18:54:13 +0530

    print statement fixed.

--- Midlabs.py
+++ Midlabs.py
@@ -3,7 +3,7 @@
 adjective = input ("Please enter an adjective")
 Noun  = input("Pkease enter a noun")
 verb_pasttense  = input("Please enter a verb past tense")
-adverb = print ("Please enter an adverb")
+adverb = input ("Please enter an adverb")
 
 
 midlabs = f"Today I went to the zoo. I saw {adjective}   {Noun}  jumping up and down in its tree. He {verb_pasttense} {adverb}."
