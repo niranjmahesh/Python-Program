50a972970b777837f86f132060d18148aff3ecb3
niranjmahesh <niranjmahesh@gmail.com>
2024-12-29 18:39:08 +0530

    Midlabs code

---
+++ Midlabs.py
@@ -0,0 +1,11 @@
+#Today I went to the zoo. I saw a(n). ______(adjective). ______(Noun) jumping up and down in its tree. He ______(verb, past tense) ______(adverb).
+
+adjective = input ("Please enter an adjective")
+Noun  = input("Pkease enter a noun")
+verb_pasttense  = input("Please enter a verb past tense")
+adverb = print ("Please enter an adverb")
+
+
+midlabs = f"Today I went to the zoo. I saw {adjective}   {Noun}  jumping up and down in its tree. He {verb_pasttense} {adverb}."
+
+print(midlabs)
